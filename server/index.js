257de89c48c830a12f1dc02d70e60da129bcf05e
ankitasahu2911 <145644574+ankitasahu2911@users.noreply.github.com>
2025-07-02 19:57:43 +0530

    first commit: "Day 1: Setup MERN project with Vite + Tailwind + Node.js + MongoDB"

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("AllyNet API running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}).catch(err => console.log(err));
